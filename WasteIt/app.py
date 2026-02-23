98c9d64d9715586b33a759738ab0bd237ffe596f
mondares <m0nndares@gmail.com>
2026-02-23 14:56:54 -0600

    back2render (railway isnt free at all)

--- WasteIt/app.py
+++ WasteIt/app.py
@@ -86,5 +86,6 @@ def predict():
         return jsonify({'error': f"Error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    # port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
